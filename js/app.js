795df4b843b4766fe880fe7e27bc301eb7420c07
Ryan Rotz <ryanrotz@gmail.com>
2016-10-03 17:36:30 -0700

    getting correct response from meetup api. Changed URL from .xml to .json

--- js/app.js
+++ js/app.js
@@ -1,13 +1,38 @@
 angular.module('ThisWeekApp', [])
 
 
-.controller('HomeCtrl', [$scope, function($scope) {
+.controller('HomeCtrl', ['$scope', '$http', function($scope, $http) {
 
-  $scope.city = '';
-
+  $scope.zip = '98122';
+  $scope.radius = '5';
+  $scope.text = 'tech';
+  $scope.data = [];
 
   $scope.submitCity = function() {
+    var req = {
+      url: 'https://api.meetup.com/2/open_events.json?',
+      method: 'GET',
+      params: {
+        'zip': $scope.zip,
+        'radius': $scope.radius,
+        'text': $scope.text,
+        'time': ',1w',
+        'key': 'ADD KEY HERE'
+        // page: 10
+        // key: process.env.MEETUP_SECRET
+      }
+    }
 
+  $http(req).then(function success(res) {
+     // var data = JSON.parse(res.data) 
+      // var data2 = res.data.config;   
+      // console.log(res);
+      console.log(res);
+      // console.log(data2);
+  }, function error(res) {
+
+      console.log(res);
+    });
   };
 
 }]);
