6b8e77792c7784a322fa4ee6e0472f4355d989ad
Ryan Rotz <ryanrotz@gmail.com>
2016-11-17 14:37:01 -0800

    removed child controller. Gave 3 columns their own separate controllers. Swiping isn't working.

--- public/app/app.js
+++ public/app/app.js
@@ -61,7 +61,7 @@ myApp.controller('ParentCtrl', ['$scope', '$http', function($scope, $http) {
   }
 
   $scope.submitCity = function() {
-    // console.log($scope.zip)
+
     var req = {
       url: 'https://api.meetup.com/2/open_events.json?',
       method: 'GET',
@@ -104,6 +104,234 @@ myApp.controller('ParentCtrl', ['$scope', '$http', function($scope, $http) {
 
 }]);
 
-myApp.controller('ChildCtrl', ['$scope', '$http', function($scope, $http) {
+myApp.controller('ColumnOneCtrl', ['$scope', '$http', function($scope, $http) {
+
+  $scope.zip = '';
+  $scope.radius = '5';
+  $scope.text = '';
+  $scope.meetups = [];
+  $scope.topics = [
+    'Outdoors',
+    'Tech',
+    'Moms',
+    'Well-Being',
+    'Sports',
+    'Education',
+    'Photography',
+    'Food',
+    'Writing',
+    'Language',
+    'Music',
+    'Movements',
+    'LGBTQ',
+    'Film',
+    'Games',
+    'Beliefs',
+    'Arts',
+    'Fashion',
+    'Social',
+    'Career'
+  ]
+
+  $scope.bindTopic = function(topic) {
+    $scope.text = topic;
+  }
+
+  $scope.submitCity = function() {
+
+    var req = {
+      url: 'https://api.meetup.com/2/open_events.json?',
+      method: 'GET',
+      params: {
+        'zip': $scope.zip,
+        'radius': $scope.radius,
+        'text': $scope.text,
+        'time': ',1w',
+        'key': '6e3f56396c8665c5e6e5d4d78653828'
+        // page: 10
+        // key: process.env.MEETUP_SECRET
+      }
+    }
+
+  $http(req).then(function success(res) {
+     $scope.meetups = res.data.results;
+      console.log(res);
+      // console.log(data2);
+  }, function error(res) {
+      console.log(res);
+    });
+  };
+
+ $scope.showActions = false;
+  
+  $scope.someFunction = function(){
+    $scope.showActions = !$scope.showActions;
+  };
+
+  
+  $scope.showMe = false;
+  $scope.showMore = true;
+
+  $scope.swipe = function(){
+    // columns move to the left
+    // the column on the right appears
+    $scope.showMe = !$scope.showMe;
+    $scope.showMore = !$scope.showMore;
+  };
+
+}]);
+
+
+myApp.controller('ColumnTwoCtrl', ['$scope', '$http', function($scope, $http) {
+
+  $scope.zip = '';
+  $scope.radius = '5';
+  $scope.text = '';
+  $scope.meetups = [];
+  $scope.topics = [
+    'Outdoors',
+    'Tech',
+    'Moms',
+    'Well-Being',
+    'Sports',
+    'Education',
+    'Photography',
+    'Food',
+    'Writing',
+    'Language',
+    'Music',
+    'Movements',
+    'LGBTQ',
+    'Film',
+    'Games',
+    'Beliefs',
+    'Arts',
+    'Fashion',
+    'Social',
+    'Career'
+  ]
+
+  $scope.bindTopic = function(topic) {
+    $scope.text = topic;
+  }
+
+  $scope.submitCity = function() {
+
+    var req = {
+      url: 'https://api.meetup.com/2/open_events.json?',
+      method: 'GET',
+      params: {
+        'zip': $scope.zip,
+        'radius': $scope.radius,
+        'text': $scope.text,
+        'time': ',1w',
+        'key': '6e3f56396c8665c5e6e5d4d78653828'
+        // page: 10
+        // key: process.env.MEETUP_SECRET
+      }
+    }
+
+  $http(req).then(function success(res) {
+     $scope.meetups = res.data.results;
+      console.log(res);
+      // console.log(data2);
+  }, function error(res) {
+      console.log(res);
+    });
+  };
+
+ $scope.showActions = false;
+  
+  $scope.someFunction = function(){
+    $scope.showActions = !$scope.showActions;
+  };
+
+  
+  $scope.showMe = false;
+  $scope.showMore = true;
+
+  $scope.swipe = function(){
+    // columns move to the left
+    // the column on the right appears
+    $scope.showMe = !$scope.showMe;
+    $scope.showMore = !$scope.showMore;
+  };
+
+}]);
+
+myApp.controller('ColumnThreeCtrl', ['$scope', '$http', function($scope, $http) {
+
+  $scope.zip = '';
+  $scope.radius = '5';
+  $scope.text = '';
+  $scope.meetups = [];
+  $scope.topics = [
+    'Outdoors',
+    'Tech',
+    'Moms',
+    'Well-Being',
+    'Sports',
+    'Education',
+    'Photography',
+    'Food',
+    'Writing',
+    'Language',
+    'Music',
+    'Movements',
+    'LGBTQ',
+    'Film',
+    'Games',
+    'Beliefs',
+    'Arts',
+    'Fashion',
+    'Social',
+    'Career'
+  ]
+
+  $scope.bindTopic = function(topic) {
+    $scope.text = topic;
+  }
+
+  $scope.submitCity = function() {
+
+    var req = {
+      url: 'https://api.meetup.com/2/open_events.json?',
+      method: 'GET',
+      params: {
+        'zip': $scope.zip,
+        'radius': $scope.radius,
+        'text': $scope.text,
+        'time': ',1w',
+        'key': '6e3f56396c8665c5e6e5d4d78653828'
+        // page: 10
+        // key: process.env.MEETUP_SECRET
+      }
+    }
+
+  $http(req).then(function success(res) {
+     $scope.meetups = res.data.results;
+      console.log(res);
+      // console.log(data2);
+  }, function error(res) {
+      console.log(res);
+    });
+  };
+
+ $scope.showActions = false;
+  
+  $scope.someFunction = function(){
+    $scope.showActions = !$scope.showActions;
+  };
+
+  
+  $scope.showMe = false;
+  $scope.showMore = true;
+
+  $scope.swipe = function(){
+    // columns move to the left
+    // the column on the right appears
+    $scope.showMe = !$scope.showMe;
+    $scope.showMore = !$scope.showMore;
+  };
 
 }]);
