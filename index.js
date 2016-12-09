11d84cbd113318125e1deb6858a55bf9566a6cbd
Ryan Rotz <ryanrotz@gmail.com>
2016-12-09 14:53:40 -0800

    getting ready to for initial push to heroku

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-var server = app.listen(process.env.PORT || 3000);
+var server = app.listen(process.env.PORT || 3001);
 
 module.exports = server;
